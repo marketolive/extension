37c93ace2ad6849d506f529a752ffbbfc010255a
Andrew Garcia <andrew@Andrews-MacBook-Pro-2.local>
2015-06-12 16:11:42 -0700

    Updated plugin for opening links in the popup menu

---
+++ pluginv3/chrome-extension/popup.js
@@ -0,0 +1,22 @@
+/**************************************************************************************
+ *
+ *  For whatever reason, Chrome does not allow the opening of links from inside
+ *  popup.html. The workaround for this is contained below. We select all of the
+ *  <a> tags, and then add a click listener that calls window.open() on the <a>
+ *  tag's target URL. Jquery is also not allowed by Chrome in this context.
+ *
+ *  @Author Andy
+ *
+ *  @function
+ *
+ **************************************************************************************/
+window.onload = function () {
+    tags = document.getElementsByClassName("link");
+    // getElementsByClassName() returns an array, so the click
+    // listener needs to be added to each one individually.
+    for (var ii = 0; ii < tags.length; ++ii) {
+        tags[ii].onclick = function () {
+            window.open(this.href);
+        }
+    }
+}
