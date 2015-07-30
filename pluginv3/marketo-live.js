d6fa4123f3f59ca3a12b1b8c85b306aba6aebbe4
unknown <angarcia@ANGARCIA-3134.marketo.net>
2015-07-29 19:46:58 -0700

    Added a wait step for the PODS namespace

--- pluginv3/marketo-live.js
+++ pluginv3/marketo-live.js
@@ -88,15 +88,18 @@ LIVE.displayLoginMessage = function () {
  *  Main
  *
  **************************************************************************************/
+var isPodsLoaded = window.setInterval(function() {
+        if (typeof(PODS) !== "undefined") {
+            console.log("Marketo Live > Finished: Waiting for PODS")
+            var podString = PODS.getCookie("userPod");
+            if (!podString) {
+                LIVE.displayLoginMessage();
+            }
 
-console.log("Marketo Live > Running: Main");
+            var pod = new PODS.Pod(podString);
+            if (currentUrl.search(mktoLiveDomain) != -1) {
+                LIVE.insertDeepLinks(pod);
+            }
+        }
+});
 
-var podString = PODS.getCookie("userPod");
-if (!podString) {
-    LIVE.displayLoginMessage();
-}
-
-var pod = new PODS.Pod(podString);
-if (currentUrl.search(mktoLiveDomain) != -1) {
-    LIVE.insertDeepLinks(pod);
-}
