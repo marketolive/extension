a1030e5c814cacc8a600ee992bbb7051130eed21
unknown <angarcia@ANGARCIA-3134.marketo.net>
2015-07-30 14:56:31 -0700

    Fixed error in goagile.js

--- website/assets/js/goagile.js
+++ website/assets/js/goagile.js
@@ -1,40 +1,60 @@
+$(window).resize(function() {
+    var windowHeight = $(window).height();
+    $('#main-body').css({
+        'height': windowHeight
+    });
+    $('#partdeux').css({
+        'height': windowHeight
+    });
+    $('#parttrois').css({
+        'height': windowHeight
+    });
 
-$( window ).resize(function() {
-  var windowHeight = $(window).height();
-  console.log(windowHeight);
-  $('#main-body').css({'height': windowHeight});
-  $('#partdeux').css({'height': windowHeight});
-  $('#parttrois').css({'height': windowHeight});
-
-  var updateHeight = function(ele, height){
-    ele = '#' + ele;
-    var thirdHeight = $( window ).height() * 1/3;
-    var elementOffSet = $(ele).offset().top;
-    var totalHeight = thirdHeight + elementOffSet - height;
-    var mainContainer = '.marketo-live-guide-option-container-home-next';
-    $(ele).find(mainContainer).css({'margin-top': height,'position':'absolute', 'top': totalHeight, 'left':'0%'});
-  }
-  updateHeight('main-body', '0');
-  updateHeight('partdeux', '50');
-  updateHeight('parttrois', '50');
+    var updateHeight = function(ele, height) {
+        ele = '#' + ele;
+        var thirdHeight = $(window).height() * 1 / 3;
+        var elementOffSet = $(ele).offset().top;
+        var totalHeight = thirdHeight + elementOffSet - height;
+        var mainContainer = '.marketo-live-guide-option-container-home-next';
+        $(ele).find(mainContainer).css({
+            'margin-top': height,
+            'position': 'absolute',
+            'top': totalHeight,
+            'left': '0%'
+        });
+    }
+    updateHeight('#main-body', '0');
+    updateHeight('#partdeux', '50');
+    updateHeight('#parttrois', '50');
 });
-$( window ).ready(function() {
-  var windowHeight = $(window).height();
-  //console.log(windowHeight);
-  $('#main-body').css({'height': windowHeight});
-  $('#partdeux').css({'height': windowHeight});
-  $('#parttrois').css({'height': windowHeight});
-  var updateHeight = function(ele){
-    ele = '#' + ele;
-    var thirdHeight = $( window ).height() * 1/3 ;
-    var elementOffSet = $(ele).offset().top;
-    var totalHeight = thirdHeight + elementOffSet;
-    var mainContainer = '.marketo-live-guide-option-container-home-next';
-    $(ele).find(mainContainer).css({'margin-top':'0px;','position':'absolute', 'top': totalHeight, 'left':'0%'});
-  }
-  updateHeight('main-body');
-  updateHeight('partdeux');
-  updateHeight('parttrois');
+$(window).ready(function() {
+    var windowHeight = $(window).height();
+    //console.log(windowHeight);
+    $('#main-body').css({
+        'height': windowHeight
+    });
+    $('#partdeux').css({
+        'height': windowHeight
+    });
+    $('#parttrois').css({
+        'height': windowHeight
+    });
+    var updateHeight = function(ele) {
+        ele = '#' + ele;
+        var thirdHeight = $(window).height() * 1 / 3;
+        var elementOffSet = $(ele).offset().top;
+        var totalHeight = thirdHeight + elementOffSet;
+        var mainContainer = '.marketo-live-guide-option-container-home-next';
+        $(ele).find(mainContainer).css({
+            'margin-top': '0px;',
+            'position': 'absolute',
+            'top': totalHeight,
+            'left': '0%'
+        });
+    }
+    updateHeight('#main-body');
+    updateHeight('#partdeux');
+    updateHeight('#parttrois');
 });
 /*
 var eleDistance = function(ele){
