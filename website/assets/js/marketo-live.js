e9dcf48731e89bac6eca7dcb964848d9a45ced0f
unknown <angarcia@ANGARCIA-3134.marketo.net>
2015-08-03 13:14:43 -0700

    Fixed loggin in marketo-live.js

--- website/assets/js/marketo-live.js
+++ website/assets/js/marketo-live.js
@@ -12,7 +12,7 @@
  *  @namespace
  *
  **************************************************************************************/
-console.log("Marketo Live > Running");
+console.log("MarketoLive > Running");
 
 var currentUrl = window.location.href,
     mktoLiveDomain = "^https:\/\/marketolive.com",
@@ -20,24 +20,6 @@ var currentUrl = window.location.href,
     tilePage = "^.*\/go-agile\/[a-zA-Z0-9]*\.html",
 
     LIVE = LIVE || {};
-
-/**************************************************************************************
- *
- *  This function displays a message to the user if he or she does not have a userPod
- *  cookie. Since the deeplinks will not work until after the first login, the user
- *  must be forced through the login page. The message instructs the user to login to 
- *  Marketo, and then return to refresh the page.
- *
- *
- *  @Author Andy
- *
- *  @function
- *
- **************************************************************************************/
-
-LIVE.displayLoginMessage = function() {
-    $("#modal-background")[0].style.display = "block";
-}
 
 /**************************************************************************************
  *
@@ -54,11 +36,11 @@ LIVE.displayLoginMessage = function() {
  **************************************************************************************/
 
 LIVE.insertDeepLinks = function(pod) {
-    console.log("Marketo Live > Inserting: Deep Links");
+    console.log("MarketoLive > Inserting: Deep Links");
 
     if (pod == null) {
         $(".marketo-live-option").click(function(e) {
-            LIVE.displayLoginMessage();
+            LIVE.displayModalWindow();
             return false;
         });
     } 
@@ -84,8 +66,8 @@ LIVE.insertDeepLinks = function(pod) {
  *
  **************************************************************************************/
 
-LIVE.displayLoginMessage = function() {
-    console.log("Marketo Live > Displaying Modal Window")
+LIVE.displayModalWindow = function() {
+    console.log("MarketoLive > Displaying Modal Window")
 
     $("#modal-background")[0].style.display = "block";
     $("#secret-passage").click(function() {
@@ -100,17 +82,20 @@ LIVE.displayLoginMessage = function() {
  **************************************************************************************/
 var isPodsLoaded = window.setInterval(function() {
     if (typeof(PODS) !== "undefined") {
-        console.log("Marketo Live > Finished: Waiting for PODS")
+        console.log("MarketoLive > Finished: Waiting for PODS");
+        
+        window.clearInterval(isPodsLoaded);
         var podString = PODS.getCookie("userPod"),
             pod = new PODS.Pod(podString);
 
         if (currentUrl.search(tilePage) != -1) {
+            console.log("MarketoLive > Location: Tile Page");
+            
             if (!podString) {
                 LIVE.insertDeepLinks(null);
             } else {
                 LIVE.insertDeepLinks(pod);
             }
         }
-        window.clearInterval(isPodsLoaded);
     }
 }, 0);
