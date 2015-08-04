2d738b6a512279237180d806917a267e1f896496
unknown <angarcia@ANGARCIA-3134.marketo.net>
2015-08-04 15:21:54 -0700

    Altered marketo-app conditional to loop for MktCanvas.activeTab instead of just MktCanvas

--- pluginv3/marketo-app.js
+++ pluginv3/marketo-app.js
@@ -836,7 +836,7 @@ if (currentUrl.search(mktoAppDomain) != -1
                     // Storing previous Workspace ID
                     if (currUrlFragment != mktoMyMarketoFragment) {
 						var isMktCanvas = window.setInterval(function() {
-							if (typeof(MktCanvas) !== "undefined") {
+							if (typeof(MktCanvas.activeTab) !== "undefined") {
 								console.log("Marketo App > Location: Marketo Canvas");
 								
 								window.clearInterval(isMktCanvas);
