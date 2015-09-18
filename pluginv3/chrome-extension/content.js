359aac1fe88cc4a0d6ad6829e0786fd1acbdab81
Andrew Garcia <angarcia@marketo.com>
2015-09-18 15:31:48 -0700

    Testing change to CSS

--- pluginv3/chrome-extension/content.js
+++ pluginv3/chrome-extension/content.js
@@ -199,6 +199,7 @@ window.onload = function() {
     
     if (currentUrl.search(mktoLiveDomain) != -1) {
         console.log("Content > Displaying Go Agile Button");
+        
         document.getElementById("first-option").style.display = "block";
     }
 
