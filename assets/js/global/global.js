9dbae2f850e7c846cd54166e811984fb21cafc29
Andrew Garcia <angarcia@marketo.com>
2015-09-18 12:13:44 -0700

    Added click listener to Get Started button

--- assets/js/global/global.js
+++ assets/js/global/global.js
@@ -1,30 +1,37 @@
-$( document ).ready(function() {
-  var x = 0;
-  $("#help-link, #sidebar-help, #audience-menu").click(function(){
-    $('.help-center-box').animate({
-      'margin-top': '125px',
-      }, 400, function() {
+$(document).ready(function() {
+    var x = 0;
+    $("#help-link, #sidebar-help").click(function() {
+        $('.help-center-box').animate({
+            'margin-top': '125px',
+        }, 400, function() {});
+        $('.help-cover').css('display', 'block');
+        $('.help-center-container').css('display', 'block');
     });
-    $('.help-cover').css('display','block');
-    $('.help-center-container').css('display','block');
-  });
-  
-    
-  $('.cancel-help').click(function(){
-    $('.help-cover').css('display','none');
-    $('.help-center-container').css('display','none');
-  });
 
-  $('.marketo-ball').click(function(){
-    if(x == 0){
-      x = 1;
-      $('.side-bar').css({'background-color':'#fff', 'z-index':'999'});
-      $('.side-bar-inner-container').fadeIn( "slow" );
-    }
-    else{
-      x = 0;
-      $('.side-bar').css({'background-color':'transparent', 'z-index':'-999'});
-      $('.side-bar-inner-container').css('display','none');      
-    }
-  });
+    $("#second-option").click(function() {
+        $("#audience-menu").css("display", "block");
+    });
+
+    $('.cancel-help').click(function() {
+        $('.help-cover').css('display', 'none');
+        $('.help-center-container').css('display', 'none');
+    });
+
+    $('.marketo-ball').click(function() {
+        if (x == 0) {
+            x = 1;
+            $('.side-bar').css({
+                'background-color': '#fff',
+                'z-index': '999'
+            });
+            $('.side-bar-inner-container').fadeIn("slow");
+        } else {
+            x = 0;
+            $('.side-bar').css({
+                'background-color': 'transparent',
+                'z-index': '-999'
+            });
+            $('.side-bar-inner-container').css('display', 'none');
+        }
+    });
 });
