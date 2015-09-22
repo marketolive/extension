ad3b86fa027722dc28b0d9969078239a4289c9aa
Andrew Garcia <angarcia@marketo.com>
2015-09-22 13:29:20 -0700

    Testing code deletion

--- assets/js/mme/mme.js
+++ assets/js/mme/mme.js
@@ -48,12 +48,6 @@ $(document).ready(function() {
         $('.side-bar').css('background-color', 'transparent');
         $('.side-bar-inner-container').css('display', 'none');
     });
-    var engagementWords = [
-        'With Marketo Mobile Engagement, designing and delivering push notifications and in-app messages is as simple as creating an email.',
-        'Beacon technology allows Mobile Apps to understand their position on a micro-local scale, and deliver hyper-contextual content to users based on location. This means users can be messaged when they pass by a location with a beacon',
-        'In-app messaging allows you to deliver engaging and relevant messages to your users.  As soon as your users open your app, they will see their personalized message.',
-        'Close the loop by synchronizing purchase data back to Marketo to report on how impactful each mobile campaign is to  your overall Marketing strategy.'
-    ]
     $('#push-container').click(function() {
         $(this).hide('slide', {
             direction: 'right'
