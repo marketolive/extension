d70eb35206cce505095e9b31290b4af47a270604
brianfisher <brian.charles.fisher@gmail.com>
2016-09-20 18:35:20 -0400

    Fix for Another LP Discard

--- pluginv3/marketo-live-app.js
+++ pluginv3/marketo-live-app.js
@@ -5105,9 +5105,8 @@ APP.getEmailIds = function(accountString) {
                             lpIds["dpageid_4876"] = "dpageid_4876";
                             lpIds["dpageid_4872"] = "dpageid_4872";
                             lpIds["dpageid_4874"] = "dpageid_4874";
-                            // Japanese DIY Design: 1, 2
+                            // Japanese DIY Design: 1
                             lpIds["dpageid_11856"] = "dpageid_11856";
-                            lpIds["dpageid_11546"] = "dpageid_11546";
                             // Japanese Event Roadshow Unknown: 1, 2
                             lpIds["dpageid_12420"] = "dpageid_12420";
                             lpIds["dpageid_12418"] = "dpageid_12418";
