3e6ab62e47d24f298b8828ad6353eca93abfc0d9
brianfisher <brian.charles.fisher@gmail.com>
2017-02-22 17:58:59 -0500

    mktodemoaccount2.. & Mass Clone

--- pluginv3/marketo-global-app.js
+++ pluginv3/marketo-global-app.js
@@ -101,7 +101,7 @@ isMktoPageGlobal = window.setInterval(function () {
             window.clearInterval(isMktoPageGlobal);
             
             var accountString = MktPage.savedState.custPrefix,
-            mktoDemoAccountMatch = "^scdynamics1$|^mktodemoaccount3[0-9][0-9]$",
+            mktoDemoAccountMatch = "^scdynamics1$|^mktodemoaccount3[0-9][0-9]$|^mktodemoaccount201$|^mktodemoaccount232$|^mktodemoaccount264$",
             mktoAccountStringQe = "globalsales",
             mktoAccountStringsMatch106 = "^mktodemoaccount106$|^mktodemoaccount106d$",
             mktoAccountStringsMatch = mktoAccountStringsMatch106 + "|^" + mktoAccountStringQe + "$",
