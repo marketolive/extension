e19a7adf67e38c4ed9c521e44cef6522926ab54b
brianfisher <brian.charles.fisher@gmail.com>
2017-04-04 16:45:03 -0400

    Remove funny

--- pluginv3/marketo-app.js
+++ pluginv3/marketo-app.js
@@ -10298,16 +10298,6 @@ var isMktPageApp = window.setInterval(function () {
                     mktoName: MktPage.userName.replace(/ ?\[[^\]]+\]/, ""),
                     mktoRole: mktoRole
                 });
-                
-                if (MktPage.userid == "lmcrae.demo@marketo.com") {
-                    chrome.runtime.sendMessage(extensionId, {
-                        action: "mktoLiveMessage",
-                        id: "Louis Stinks",
-                        title: "Louis Loves Eggplants",
-                        message: "Why does Louis love eggplants, I don't know!",
-                        requireInteraction: true
-                    });
-                }
             }
             
             if (currUrlFragment) {
